fb6f63d0f69ec412822c48d354b48184df231da6
Christian Nikolov <christian@newton.codes>
2018-01-19 09:56:57 +0200

    Iredmail install

--- iredmail/install.sh
+++ iredmail/install.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
 dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+source ${dir}/../src/lib.sh
 
 set -e
 
-echo "Domain: "
+install iredmail
+
+echo "Mail domain: "
 read domain;
 
-echo "Hostname: "
+echo "Mail hostname: "
 read hostname;
 
 mkdir -p /etc/mothership/iredmail/vpn
 cp -f ${dir}/stack.yml /etc/mothership/iredmail/stack.yml
 
 sed -i "s/- DOMAIN=.*/- DOMAIN=$domain/" /etc/mothership/iredmail/stack.yml
-sed -i "s/- HOSTNAME=.*/- HOSTNAME=$hostname/" /etc/mothership/iredmail/stack.yml
+sed -i "s/- HOSTNAME=.*/- HOSTNAME=$hostname/" /etc/mothership/iredmail.yml
 
 MYSQL_ROOT_PASSWORD=$(pwgen -1 32)
 POSTMASTER_PASSWORD=$(pwgen -1 32)
@@ -22,7 +25,6 @@ POSTMASTER_PASSWORD=$(pwgen -1 32)
 docker volume create iredmail_vmail > /dev/null
 docker volume create iredmail_clamav > /dev/null
 docker volume create iredmail_mysql > /dev/null
-docker network create --attachable iredmail > /dev/null
 
 docker run --privileged --rm -p 8881:80 -p 8882:443 \
            -e "DOMAIN=$domain"
