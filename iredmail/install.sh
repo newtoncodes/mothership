70e0037ad6056bad9edffe8c3a1f404accbeadb9
Christian Nikolov <christian@newton.codes>
2018-01-19 14:05:35 +0200

    Debugs

--- iredmail/install.sh
+++ iredmail/install.sh
@@ -3,21 +3,29 @@
 dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 source ${dir}/../src/lib.sh
 
+docker volume rm iredmail_vmail > /dev/null
+docker volume rm iredmail_clamav > /dev/null
+docker volume rm iredmail_mysql > /dev/null
+domain=newton.codes
+hostname=mail
+
 set -e
 
 install iredmail
 
 echo "Mail domain: "
-read domain;
+#read domain;
 
 echo "Mail hostname: "
-read hostname;
+#read hostname;
 
 sed -i "s/- DOMAIN=.*/- DOMAIN=$domain/" /etc/mothership/iredmail.yml
 sed -i "s/- HOSTNAME=.*/- HOSTNAME=$hostname/" /etc/mothership/iredmail.yml
 
-MYSQL_ROOT_PASSWORD=$(pwgen -1 32)
-POSTMASTER_PASSWORD=$(pwgen -1 32)
+MYSQL_ROOT_PASSWORD=parola123
+#$(pwgen -1 32)
+POSTMASTER_PASSWORD=parola123
+#$(pwgen -1 32)
 
 docker volume create iredmail_vmail > /dev/null
 docker volume create iredmail_clamav > /dev/null
@@ -35,21 +43,20 @@ docker run --privileged --rm -d -p 8881:80 -p 8882:443 \
     --name=iredmail_tmp \
 newtoncodes/iredmail:0.9.7
 
-
 id=$(docker ps | grep iredmail_tmp | awk '{print $1;}')
+
+set +e
 
 echo "Iredmail init process in progress..."
 ready=
 
-for i in {30..0}; do
-    sleep 1
+for i in {40..0}; do
+    sleep 5
 
     if [ "$ready" != "" ]; then break; fi
 
     ready=$(docker logs ${id} 2> /dev/null | grep "Self checking every 3600 seconds.")
 done
-
-set +e
 
 if [ "$i" = 0 ]; then
     docker stop ${id} > /dev/null
@@ -59,9 +66,12 @@ fi
 
 echo "Iredmail is ready."
 
-docker stop ${id} > /dev/null
+#docker stop ${id} > /dev/null
 
 echo ""
 echo "Root: $MYSQL_ROOT_PASSWORD"
 echo "Postmaster: $POSTMASTER_PASSWORD"
 echo ""
+
+docker logs ${id}
+docker exec -it ${id} /bin/bash
