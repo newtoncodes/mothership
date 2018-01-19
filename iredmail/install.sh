04171cecd44b418c995c03fc8911305b744b537f
Christian Nikolov <christian@newton.codes>
2018-01-19 10:07:38 +0200

    Fix iredmail docker run command

--- iredmail/install.sh
+++ iredmail/install.sh
@@ -24,14 +24,15 @@ docker volume create iredmail_clamav > /dev/null
 docker volume create iredmail_mysql > /dev/null
 
 docker run --privileged --rm -p 8881:80 -p 8882:443 \
-           -e "DOMAIN=$domain"
-           -e "HOSTNAME=$hostname" \
-           -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" \
-           -e "POSTMASTER_PASSWORD=$POSTMASTER_PASSWORD" \
-           -e "IREDAPD_PLUGINS=['reject_null_sender', 'reject_sender_login_mismatch', 'greylisting', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
-           -v iredmail_mysql:/var/lib/mysql \
-           -v iredmail_vmail:/var/vmail \
-           -v iredmail_clamav:/var/lib/clamav \
-           --name=iredmail_tmp lejmr/iredmail:mysql-latest
+    -e "DOMAIN=$domain" \
+    -e "HOSTNAME=$hostname" \
+    -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" \
+    -e "POSTMASTER_PASSWORD=$POSTMASTER_PASSWORD" \
+    -e "IREDAPD_PLUGINS=['reject_null_sender', 'reject_sender_login_mismatch', 'greylisting', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
+    -v iredmail_mysql:/var/lib/mysql \
+    -v iredmail_vmail:/var/vmail \
+    -v iredmail_clamav:/var/lib/clamav \
+    --name=iredmail_tmp \
+newtoncodes/iredmail:0.9.7
 
 id=$(docker ps | grep iredmail_tmp | awk '{print $1;}')
