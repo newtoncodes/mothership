83331d2f2a09e85b95b2064a32c2c3373eec59ed
Christian Nikolov <christian@newton.codes>
2018-01-19 07:37:10 +0200

    Fix webserver install

--- webserver/install.sh
+++ webserver/install.sh
@@ -8,10 +8,10 @@ mkdir -p /etc/mothership/templates
 mkdir -p /etc/mothership/vhosts
 mkdir -p /etc/mothership/vpn
 
-cp -f ${dir}/webserver/stack.yml "/etc/mothership/webserver.yml"
+cp -f ${dir}/stack.yml "/etc/mothership/webserver.yml"
 
-cp -f ${dir}/start.sh "/usr/local/bin/mothership-webserver-start"
-cp -f ${dir}/start.sh "/usr/local/bin/mothership-webserver-stop"
+cp -f ${dir}/../src/start.sh "/usr/local/bin/mothership-webserver-start"
+cp -f ${dir}/../src/start.sh "/usr/local/bin/mothership-webserver-stop"
 
 sed -i "s/{{app}}/webserver/" "/usr/local/bin/mothership-webserver-start"
 sed -i "s/{{app}}/webserver/" "/usr/local/bin/mothership-webserver-stop"
