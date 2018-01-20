52a55a17debc0976eef4d247397055d380323693
Christian Nikolov <christian@newton.codes>
2018-01-20 22:10:45 +0200

    Better registry install, add and remove users

--- registry/install.sh
+++ registry/install.sh
@@ -5,8 +5,23 @@ source ${dir}/../src/lib.sh
 
 set -e
 
-install registry
+mkdir -p /etc/mothership/registry-auth
 
+install registry
 
 docker volume create mothership_registry_data > /dev/null
 
+echo "#!/usr/bin/env bash
+
+docker run \
+  --entrypoint htpasswd \
+  registry:2 -Bbn $1 $2 >> /etc/mothership/registry-auth/htpasswd
+" > /usr/local/bin/mothership-registry-user-add
+
+echo "#!/usr/bin/env bash
+
+sed -i \"s@^${1}:.*@@\" /etc/mothership/registry-auth/htpasswd
+" > /usr/local/bin/mothership-registry-user-remove
+
+chmod +x /usr/local/bin/mothership-registry-user-add
+chmod +x /usr/local/bin/mothership-registry-user-remove
