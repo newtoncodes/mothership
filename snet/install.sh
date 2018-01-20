4db897ef8820277ebf4c073c845b561c8676b07d
Christian Nikolov <christian@newton.codes>
2018-01-20 19:42:06 +0200

    Install snet script

---
+++ snet/install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+
+set -e
+
+mkdir -p /etc/mothership/vpn
+
+
+echo "#!/usr/bin/env bash
+
+dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+
+docker run \
+    --detach \
+    --rm \
+    --network=mothership \
+    --name mothership_snet \
+    --hostname snet \
+    --restart always \
+    --cap-add=NET_ADMIN \
+    --device=/dev/net/tun \
+    -v /etc/mothership/vpn:/etc/snet \
+    -e PORTS=80:nginx:80 443:nginx:443 22:gitlab:22
+newtoncodes/snet:1.0.0
+
+" > /usr/local/bin/mothership-snet-start
+
+echo "#!/usr/bin/env bash
+
+dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+
+docker stop $(docker ps -aq --filter=name=mothership_snet)
+" > /usr/local/bin/mothership-snet-stop
+
+chmod +x "/usr/local/bin/mothership-snet-start"
+chmod +x "/usr/local/bin/mothership-snet-stop"
