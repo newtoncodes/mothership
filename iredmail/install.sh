35360527d1e3829a485862dd90c224267d3b7408
Christian Nikolov <christian@newton.codes>
2018-01-20 06:53:48 +0200

    Fix iredmail volumes prefix

--- iredmail/install.sh
+++ iredmail/install.sh
@@ -19,9 +19,9 @@ sed -i "s/- HOSTNAME=.*/- HOSTNAME=$hostname/" /etc/mothership/iredmail.yml
 MYSQL_ROOT_PASSWORD=$(pwgen -1 32)
 POSTMASTER_PASSWORD=$(pwgen -1 32)
 
-docker volume create iredmail_vmail > /dev/null
-docker volume create iredmail_clamav > /dev/null
-docker volume create iredmail_mysql > /dev/null
+docker volume create mothership_iredmail_vmail > /dev/null
+docker volume create mothership_iredmail_clamav > /dev/null
+docker volume create mothership_iredmail_mysql > /dev/null
 
 docker run --privileged --rm -d -p 8881:80 -p 8882:443 \
     -e "DOMAIN=$domain" \
@@ -29,9 +29,9 @@ docker run --privileged --rm -d -p 8881:80 -p 8882:443 \
     -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" \
     -e "POSTMASTER_PASSWORD=$POSTMASTER_PASSWORD" \
     -e "IREDAPD_PLUGINS=['reject_null_sender', 'reject_sender_login_mismatch', 'greylisting', 'throttle', 'amavisd_wblist', 'sql_alias_access_policy']" \
-    -v iredmail_mysql:/var/lib/mysql \
-    -v iredmail_vmail:/var/vmail \
-    -v iredmail_clamav:/var/lib/clamav \
+    -v mothership_iredmail_mysql:/var/lib/mysql \
+    -v mothership_iredmail_vmail:/var/vmail \
+    -v mothership_iredmail_clamav:/var/lib/clamav \
     --name=iredmail_tmp \
 newtoncodes/iredmail:0.9.7
 
