6c26f622b3382e5b73a154af35239705d4a7a2ff
Christian Nikolov <christian@newton.codes>
2018-01-22 05:13:25 +0200

    Registry install

--- apps/registry/install.sh
+++ apps/registry/install.sh
@@ -1,35 +1,23 @@
 #!/usr/bin/env bash
 
 dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-source ${dir}/../src/lib.sh
+source ${dir}/../../src/lib.sh
 
 set -e
 
-mkdir -p /etc/mothership/registry-auth
-
 install registry
 
-docker volume create mothership_registry_data > /dev/null
+docker volume create registry_data > /dev/null
+docker volume create registry_config > /dev/null
+docker volume create registry_auth_config > /dev/null
+docker volume create registry_auth_log > /dev/null
 
-echo "#!/usr/bin/env bash
-
-docker run \\
-  --entrypoint htpasswd \\
-  registry:2 -Bbn \${1} \${2} >> /etc/mothership/registry-auth/htpasswd
-
-sed -i -n '/./,/^$/p' /etc/mothership/registry-auth/htpasswd
-sed -i -n '/./,/^$/p' /etc/mothership/registry-auth/htpasswd
-" > /usr/local/bin/mothership-registry-user-add
-
-echo "#!/usr/bin/env bash
-
-sed -i \"s@^\${1}:.*@@\" /etc/mothership/registry-auth/htpasswd
-sed -i -n '/./,/^$/p' /etc/mothership/registry-auth/htpasswd
-sed -i -n '/./,/^$/p' /etc/mothership/registry-auth/htpasswd
-" > /usr/local/bin/mothership-registry-user-remove
-
-chmod +x /usr/local/bin/mothership-registry-user-add
-chmod +x /usr/local/bin/mothership-registry-user-remove
+mv ${dir}/registry.yml /var/lib/docker/volumes/registry_config/_data/config.yml
+mv ${dir}/auth.ldap.yml /var/lib/docker/volumes/registry_auth_config/_data/auth.ldap.yml
+mv ${dir}/auth.simple.yml /var/lib/docker/volumes/registry_auth_config/_data/auth.simple.yml
 
 
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout auth.key -x509 -days 365 -out auth.crt
+#openssl req -newkey rsa:4096 -nodes -sha256 -keyout /tmp/auth.key -x509 -days 365 -out /tmp/auth.crt
+
+#mv /tmp/auth.crt /var/lib/docker/volumes/registry_auth_config/_data/auth.crt
+#mv /tmp/auth.key /var/lib/docker/volumes/registry_auth_config/_data/auth.key
