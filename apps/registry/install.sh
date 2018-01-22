6c4478acc8bd6edd632b503ac0a3bf39fecbb55a
Christian Nikolov <christian@newton.codes>
2018-01-22 07:05:10 +0200

    Remove debugs from install script

--- apps/registry/install.sh
+++ apps/registry/install.sh
@@ -8,28 +8,28 @@ set -e
 domain2=
 install registry foo yes
 
-#echo "LDAP users dn: "
-#read ldapUsersDn;
-#
-#echo "LDAP search dn: "
-#read ldapSearchDn;
-#
-#docker volume create registry_data > /dev/null
-#docker volume create registry_config > /dev/null
-#docker volume create registry_auth_config > /dev/null
-#docker volume create registry_auth_log > /dev/null
+echo "LDAP users dn: "
+read ldapUsersDn;
 
-cp ${dir}/registry.yml /var/lib/docker/volumes/registry_config/_data/config.yml
-#cp ${dir}/auth.ldap.yml /var/lib/docker/volumes/registry_auth_config/_data/auth.ldap.yml
-#cp ${dir}/auth.simple.yml /var/lib/docker/volumes/registry_auth_config/_data/auth.simple.yml
+echo "LDAP search dn: "
+read ldapSearchDn;
 
-#sed -i "s/{{SEARCH_DN}}/$ldapSearchDn/" /var/lib/docker/volumes/registry_auth_config/_data/auth.ldap.yml
-#sed -i "s/{{USERS_DN}}/$ldapUsersDn/" /var/lib/docker/volumes/registry_auth_config/_data/auth.ldap.yml
+docker volume create registry_data > /dev/null
+docker volume create registry_config > /dev/null
+docker volume create registry_auth_config > /dev/null
+docker volume create registry_auth_log > /dev/null
+
+cp -f ${dir}/registry.yml /var/lib/docker/volumes/registry_config/_data/config.yml
+cp -f ${dir}/auth.ldap.yml /var/lib/docker/volumes/registry_auth_config/_data/auth.ldap.yml
+cp -f ${dir}/auth.simple.yml /var/lib/docker/volumes/registry_auth_config/_data/auth.simple.yml
+
+sed -i "s/{{SEARCH_DN}}/$ldapSearchDn/" /var/lib/docker/volumes/registry_auth_config/_data/auth.ldap.yml
+sed -i "s/{{USERS_DN}}/$ldapUsersDn/" /var/lib/docker/volumes/registry_auth_config/_data/auth.ldap.yml
 sed -i "s/{{HOST}}/$domain2/" /var/lib/docker/volumes/registry_config/_data/config.yml
 
-#ldapSearchPassword=$(pwgen -1 32)
-#echo "$ldapSearchPassword" > /var/lib/docker/volumes/registry_auth_config/_data/bind-password.txt
+ldapSearchPassword=$(pwgen -1 32)
+echo "$ldapSearchPassword" > /var/lib/docker/volumes/registry_auth_config/_data/bind-password.txt
 
-#echo ""
-#echo "Search dn password: $ldapSearchPassword"
-#echo ""
+echo ""
+echo "Search dn password: $ldapSearchPassword"
+echo ""
